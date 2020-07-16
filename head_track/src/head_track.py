a648769fbac0be1c44e4d7be2d6e25ece354ad42
sLaderoute <66643113+sLaderoute@users.noreply.github.com>
2020-07-16 16:23:23 -0400

    supports multiple tracking points
    does outside in head tracking. requires specific tracking markers

--- head_track/src/head_track.py
+++ head_track/src/head_track.py
@@ -8,38 +8,27 @@ import rospy, cv2.aruco, numpy, cv2, sys, time, math, tf, sensor_msgs.msg, geome
 from cv_bridge import CvBridge
 from std_msgs.msg import Header as HeaderMsg
 import numpy as np
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import copy
 import array
 
 # define tag
 id_to_find = 1
-marker_size = 0.10  # m
+marker_size = 0.04  # m
+id_back = 380
+id_back_right = 403
+id_right = 643
+id_front_right_1 = 303
+id_front_right_2 = 473
+id_front = 688
 
-# initalize these values here (will be in init)
+# initialize these values here (will be in init)
 tvec = [0, 0, 0]
 q = [0, 0, 0, 1]
-# used for averaging -- not used
-# def_pose = geometry_msgs.msg.PoseStamped()
-# header = HeaderMsg()
-# header.frame_id = '/camera_link'
-# header.stamp = 0
-# def_pose.header = header
-# def_pose.pose.position.x = 0
-# def_pose.pose.position.y = 0
-# def_pose.pose.position.z = 0
-# def_pose.pose.orientation.x = 0
-# def_pose.pose.orientation.y = 0
-# def_pose.pose.orientation.z = 0
-# def_pose.pose.orientation.w = 1
-# pose_arr = []
-# i = 0
-# while i < 10:
-#     pose_arr.insert(0, def_pose)
-#     i += 1
+last_q = [0,0,0,1]
+last_tvec = [0,0,0]
 
 # get camera calibration
-
-
 calib_path = '/home/csrobot/catkin_ws/src/head_track/calibration/'
 camera_matrix = numpy.loadtxt(calib_path + 'cameraMatrix.txt', delimiter=',')
 camera_distortion = numpy.loadtxt(calib_path + 'cameraDistortion.txt', delimiter=',')
@@ -63,24 +52,11 @@ pubPose = rospy.Publisher("/head_pose", geometry_msgs.msg.PoseStamped, queue_siz
 
 pose = geometry_msgs.msg.PoseStamped()
 
-
 def aa2quat(aa):
-    # print 'aa: ', aa
     angle = np.linalg.norm(aa)
     axis = (aa[0] / angle, aa[1] / angle, aa[2] / angle)
     angle_2 = angle / 2
-    q = (axis[0] * np.sin(angle_2), axis[1] * np.sin(angle_2), axis[2] * np.sin(angle_2), np.cos(angle_2))
-    return q
-
-
-# used for averaging -- not used
-# def reject_outliers(dataIn, factor=0.8):
-#     quant3, quant1 = np.percentile(dataIn, [75, 25])
-#     iqr = quant3 - quant1
-#     iqrSigma = iqr / 1.34896
-#     medData = np.median(dataIn)
-#     dataOut = [x for x in dataIn if ((x > medData - factor * iqrSigma) and (x < medData + factor * iqrSigma))]
-#     return dataOut
+    return [axis[0] * np.sin(angle_2), axis[1] * np.sin(angle_2), axis[2] * np.sin(angle_2), np.cos(angle_2)]
 
 
 def callback(rawFrame):  # gets frame from realsense
@@ -88,16 +64,11 @@ def callback(rawFrame):  # gets frame from realsense
     global tvec
     global q
     global pose_arr
+    global last_q
+    global last_tvec
 
     # capture video camera frame
-    # cap = bridge.imgmsg_to_cv2(rawFrame, "bgr8") #desired_encoding='passthrough')
     frame = bridge.imgmsg_to_cv2(rawFrame, "bgr8")
-    # set camera size
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    # read camera frame
-    # ret, frame = cap.read()
 
     # grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -106,25 +77,78 @@ def callback(rawFrame):  # gets frame from realsense
     corners, ids, rejected = cv2.aruco.detectMarkers(image=gray, dictionary=aruco_dict, parameters=parameters,
                                                      cameraMatrix=camera_matrix, distCoeff=camera_distortion)
 
-    if ids is not None and ids[0] == id_to_find:
+    if ids is not None:  # and ids[0] == id_to_find:
         # ret = [rvec, tvec, ?]
         # array of rotation and position of each marker
         # rvec = [[rvec1],[rvec2],...] attitude
         # tvec = [[tvec1].[tvec2],...] position
-        ret = cv2.aruco.estimatePoseSingleMarkers(corners, marker_size, camera_matrix, camera_distortion)
 
-        # unpack output only get first
-        rvec, tvec = ret[0][0, 0, :], ret[1][0, 0, :]  # looks like this can be eliminated if we put the two vars into
-        # the above function call, doesnt hurt us to see more markers?
+        for i in range(0, len(ids)):
+            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], marker_size, camera_matrix,
+                                                                           camera_distortion)
+            # draw the marker and put reference frame
+            cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+            cv2.aruco.drawAxis(frame, camera_matrix, camera_distortion, rvec, tvec, marker_size / 2)
 
-        # draw the marker and put reference frame
-        cv2.aruco.drawDetectedMarkers(frame, corners)
-        cv2.aruco.drawAxis(frame, camera_matrix, camera_distortion, rvec, tvec, marker_size / 2)
+            current_id = ids[i]
+            if current_id == id_back:
+                # adjust angle 90 degrees ccw on yaw
+                # move forward 10cm
+                q = aa2quat(rvec[0][0])
+                e = euler_from_quaternion(q)
+                e = [e[0],e[1]-np.pi/2,e[2]]
+                q = quaternion_from_euler(e[0],e[1],e[2])
+                tvec = tvec[0][0]
+                tvec = [tvec[0]+0.10,tvec[1],tvec[2]]
+            elif current_id == id_back_right:
+                # adjust angle 45 degrees ccw
+                # move forward 7cm, left 6 cm
+                q = aa2quat(rvec[0][0])
+                e = euler_from_quaternion(q)
+                e = [e[0], e[1]- np.pi / 4, e[2] ]
+                q = quaternion_from_euler(e[0],e[1],e[2])
+                tvec = tvec[0][0]
+                tvec = [tvec[0] + 0.07, tvec[1] , tvec[2]- 0.06]
+            elif current_id == id_right:
+                # angle is good
+                # move left 8 cm
+                q = aa2quat(rvec[0][0])
+                tvec = tvec[0][0]
+                tvec = [tvec[0], tvec[1] , tvec[2]- 0.08]
+            elif current_id == id_front_right_1:
+                # angle rotate 30 degrees cw
+                # move backward 5 cm, left 9 cm
+                q = aa2quat(rvec[0][0])
+                e = euler_from_quaternion(q)
+                e = [e[0], e[1] + np.pi / 6, e[2]]
+                q = quaternion_from_euler(e[0], e[1], e[2])
+                tvec = tvec[0][0]
+                tvec = [tvec[0]- 0.05, tvec[1] , tvec[2]- 0.09]
+            elif current_id == id_front_right_2:
+                # angle rotate 67 degrees cw
+                # move backward 10 cm, left 5 cm
+                q = aa2quat(rvec[0][0])
+                e = euler_from_quaternion(q)
+                e = [e[0], e[1]+ 3*np.pi / 8, e[2] ]
+                q = quaternion_from_euler(e[0],e[1],e[2])
+                tvec = tvec[0][0]
+                tvec = [tvec[0] - 0.10, tvec[1] , tvec[2]- 0.05]
+            elif current_id == id_front:
+                # adjust angle 90 degrees cw
+                # move backward 10cm
+                q = aa2quat(rvec[0][0])
+                e = euler_from_quaternion(q)
+                e = [e[0], e[1]+ np.pi / 2, e[2]]
+                q = quaternion_from_euler(e[0],e[1],e[2])
+                tvec = tvec[0][0]
+                tvec = [tvec[0] - 0.10, tvec[1], tvec[2]]
+            else:
+                q = last_q
+                tvec = last_tvec
+            last_q = q
+            last_tvec = tvec
 
         # assemble pose message
-        # rospy.logwarn("rvec: %f %f %f\ntvec %d %d %d\n", rvec[0], rvec[1], rvec[2], tvec[0], tvec[1], tvec[2])
-        q = aa2quat(rvec)   # convert from axis angle to quaternion
-
         header = HeaderMsg()
         header.frame_id = '/camera_link'
         header.stamp = rospy.Time.now()
@@ -137,51 +161,6 @@ def callback(rawFrame):  # gets frame from realsense
         pose.pose.orientation.y = q[1]
         pose.pose.orientation.z = q[2]
         pose.pose.orientation.w = q[3]
-
-    # used for averaging -- not used
-    #     pose_arr.insert(0, copy.deepcopy(pose))
-    #     # print 'before'
-    #     # print pose_arr
-    #     pose_arr = pose_arr[:-1]
-    #     # print 'after'
-    #     # print pose_arr
-    #
-    # final_pose = geometry_msgs.msg.PoseStamped()
-    # header = HeaderMsg()
-    # header.frame_id = '/camera_link'
-    # header.stamp = rospy.Time.now()
-    # final_pose.header = header
-    #
-    # ox = []
-    # oy = []
-    # oz = []
-    # ow = []
-    # for poses in pose_arr:
-    #     print 'orix: ', poses.pose.orientation.x
-    #     ox.insert(0, poses.pose.orientation.x)
-    #     oy.insert(0, poses.pose.orientation.y)
-    #     oz.insert(0, poses.pose.orientation.z)
-    #     ow.insert(0, poses.pose.orientation.w)
-    #
-    # ox = reject_outliers(ox)
-    # oy = reject_outliers(oy)
-    # oz = reject_outliers(oz)
-    # ow = reject_outliers(ow)
-    #
-    # print 'xoutlie: ', ox
-    #
-    # final_pose.pose.position.x = tvec[0]
-    # final_pose.pose.position.y = tvec[1]
-    # final_pose.pose.position.z = tvec[2]
-    # final_pose.pose.orientation.x = np.mean(ox)
-    # final_pose.pose.orientation.y = np.mean(oy)
-    # final_pose.pose.orientation.z = np.mean(oz)
-    # final_pose.pose.orientation.w = np.mean(ow)
-    # print 'finorix: ', final_pose.pose.orientation.x
-    # q[0] = final_pose.pose.orientation.x
-    # q[1] = final_pose.pose.orientation.y
-    # q[2] = final_pose.pose.orientation.z
-    # q[3] = final_pose.pose.orientation.w
 
     # display frame TODO rviz this
     pubIm.publish(bridge.cv2_to_imgmsg(frame, encoding="passthrough"))
